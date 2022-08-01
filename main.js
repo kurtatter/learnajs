de69a60db2499ca693c7243792d1764976d6599c
Stas Dzasokhov <stsklv@yahoo.com>
2022-08-01 20:01:28 +0300

    <= lesson 8

---
+++ main.js
@@ -0,0 +1,38 @@
+let app = angular.module('app', []);
+
+app.factory('UserFactory', function () {
+    return {
+        name: 'Sam Flynn'
+    }
+});
+
+app.directive('hawaii', function () {
+    return function (scope, element, attrs) {
+        element.text('Hawaii');
+        element.on('click', function () {
+            element.text(element.text() === 'Hawaii' ? 'USA' : 'Hawaii');
+        });
+    };
+});
+
+app.directive('london', function() {
+    return {
+        link: function (scope, element, attrs) {
+            element.text('London');
+            element.on('click', function() {
+                element.text(element.text() === 'London' ? 'England' : 'London');
+            });
+        }
+    };
+});
+
+app.filter('moneyFilter', function () {
+    return function (str) {
+        return '$' + str;
+    };
+});
+
+app.controller('travelCtrl', function (UserFactory) {
+    this.price = 99;
+    this.UserFactory = UserFactory;
+});
